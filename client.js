37c10a46486ab1a9970cf8d7601ce8b93e7e2331
realcoloride <108619637+realcoloride@users.noreply.github.com>
2023-02-05 14:59:58 +0100

    Update client.js

--- client.js
+++ client.js
@@ -93,13 +93,15 @@ class Client {
         } else Error('Could not fetch character information.')
     }
     async searchCharacters(characterName) {
-		if (!this.isAuthenticated()) throw Error('You must be authenticated to do this.');
+        if (!this.isAuthenticated()) throw Error('You must be authenticated to do this.');
+        if (!this.#isGuest) throw Error('Guest accounts cannot use the search feature.');
         if (characterName == undefined || typeof(characterName) != 'string') throw Error('Invalid arguments.')
 
         const request = await fetch(`https://beta.character.ai/chat/characters/search/?query=${characterName}`, {
             headers:this.getHeaders()
         })
         
+        console.log(await request.text())
         if (request.status === 200) {
             const response = await request.json()
 
