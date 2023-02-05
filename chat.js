8a4fb48f87f532efc101e78569f82cdaeaa3b1bc
realcoloride <108619637+realcoloride@users.noreply.github.com>
2023-02-05 13:33:59 +0100

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,84 @@
+class Chat {
+    constructor(client, characterId, continueBody) {
+        this.characterId = characterId;
+        this.externalId = continueBody.external_id;
+
+        this.client = client;
+
+        const ai = continueBody.participants.find(
+            (participant) => participant.is_human === false
+        );
+        this.aiId = ai.user.username;
+    }
+
+    async fetchHistory() {
+        const client = this.client;
+        const request = await fetch('https://beta.character.ai/chat/history/msgs/user/?history_external_id=${this.externalId}', {
+            headers:client.getHeaders()
+        })
+
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response;
+        } else Error('Could not fetch the chat history.')
+    }
+    async sendAndAwaitResponse(message, singleReply) {
+        const payload = {
+            history_external_id: this.externalId,
+            character_external_id: this.characterId,
+            text: message,
+            tgt: this.aiId,
+            ranking_method: 'random',
+            faux_chat: false,
+            staging: false,
+            model_server_address: null,
+            override_prefix: null,
+            override_rank: null,
+            rank_candidates: null,
+            filter_candidates: null,
+            prefix_limit: null,
+            prefix_token_limit: null,
+            livetune_coeff: null,
+            stream_params: null,
+            enable_tti: true,
+            initial_timeout: null,
+            insert_beginning: null,
+            translate_candidates: null,
+            stream_every_n_steps: 16,
+            chunks_to_pad: 8,
+            is_proactive: false,
+        };
+
+        const client = this.client;
+
+        if (!client.isAuthenticated()) throw Error('You must be authenticated to do this.');
+
+        const request = await fetch('https://beta.character.ai/chat/streaming/', {
+            body:JSON.stringify(payload),
+            method:'POST',
+            headers:client.getHeaders()
+        })
+
+        if (request.status === 200) {
+            const response = await request.text()
+            const replies = [];
+
+            for (const line of response.split('\n')) {
+                if (line.startsWith('{')) {
+                  replies.push(JSON.parse(line));
+                  continue;
+                }
+          
+                const start = line.indexOf(' {');
+                if (start < 0) continue;
+                replies.push(JSON.parse(line.slice(start - 1)));
+              }
+          
+              if (!singleReply) return replies;
+              else return replies.pop().replies.shift().text;
+        } else Error('Failed sending message.')
+    }
+}
+
+module.exports = Chat
---
+++ client.js
@@ -0,0 +1,214 @@
+const Chat = require('./chat')
+const { v4: uuidv4 } = require('uuid');
+
+class Client {
+    #token = undefined;
+    #isGuest = false;
+    #authenticated = false;
+    #guestHeaders = {
+        'Content-Type': 'application/json'
+    }
+
+    constructor() {
+        this.#token = undefined;
+    }
+
+    // api fetching
+    async fetchCategories() {
+        const request = await fetch('https://beta.character.ai/chat/character/categories/')
+
+        if (request.status === 200) return await request.json();
+        else throw Error('Failed to fetch categories');
+    }
+    async fetchUserConfig() {
+        const request = await fetch('https://beta.character.ai/chat/config/', {
+            headers:this.#guestHeaders
+        })
+        
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response;
+        } else Error('Failed fetching user configuration.')
+    }
+    async fetchUser() {
+        if (!this.isAuthenticated()) throw Error('You must be authenticated to do this.');
+        
+        const request = await fetch('https://beta.character.ai/chat/user/', {
+            headers:this.getHeaders()
+        })
+
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response;
+        } else Error('Failed fetching user.')
+    }
+    async fetchFeaturedCharacters() {
+        if (!this.isAuthenticated()) throw Error('You must be authenticated to do this.');
+        
+        const request = await fetch('https://beta.character.ai/chat/characters/featured_v2/', {
+            headers:this.getHeaders()
+        })
+
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response;
+        } else Error('Failed fetching featured characters.')
+    }
+    async fetchCharactersByCategory(curated = false) {
+        if (curated == undefined || typeof(curated) != 'boolean') throw Error('Invalid arguments.')
+
+        const url = `https://beta.character.ai/chat/${
+            curated ? 'curated_categories' : 'categories'
+        }/characters/`;
+
+        const request = await fetch(url, {
+            headers:this.#guestHeaders
+        })
+
+        const property = curated
+        ? 'characters_by_curated_category'
+        : 'characters_by_category';
+      
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response[property]
+        } else Error('Failed fetching characters by category.')
+    }
+    async fetchCharacterInfo(characterId) {
+        if (characterId == undefined || typeof(characterId) != 'string') throw Error('Invalid arguments.')
+
+        const request = await fetch(`https://beta.character.ai/chat/character/info-cached/${characterId}/`, {
+            headers:this.#guestHeaders
+        })
+
+        if (request.status === 200) {
+            const response = await request.json()
+
+            return response.character;
+        } else Error('Could not fetch character information.')
+    }
+
+    // chat
+    async createOrContinueChat(characterId) {
+        if (!this.isAuthenticated()) throw Error('You must be authenticated to do this.');
+        if (characterId == undefined || typeof(characterId) != 'string') throw Error('Invalid arguments.')
+
+        let request = await fetch('https://beta.character.ai/chat/history/continue/',  {
+            body:JSON.stringify({
+                character_external_id: characterId,
+                history_external_id: null,
+            }),
+            method:'POST',
+            headers:this.getHeaders()
+        })
+
+        if (request.status === 200) {
+            let response = await request.json()
+            
+            if (response.status === "No Such History") { // Create a new chat
+                request = await fetch('https://beta.character.ai/chat/history/create/', {
+                    body:JSON.stringify({
+                        character_external_id: characterId,
+                        history_external_id: null,
+                    }),
+                    method:'POST',
+                    headers:this.getHeaders()
+                })
+                if (request.status === 200) response = await request.json()
+                else Error('Could not create a new chat.')
+            } 
+
+            // Continue it 
+            const continueBody = response;
+            return new Chat(this, characterId, continueBody)
+        } else Error('Could not create or resume a chat.')
+    }
+
+    // authentification
+    async authenticateWithToken(token) {
+        if (this.isAuthenticated()) throw Error('Already authenticated');
+        if (!token || typeof(token) != 'string') throw Error('Specify a valid token');
+
+        const request = await fetch('https://beta.character.ai/dj-rest-auth/auth0/', {
+            method:'POST',
+            body:JSON.stringify({
+                access_token: token
+            }),
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+
+        if (request.status === 200) {
+            const response = await request.json()
+
+            this.#isGuest = false;
+            this.#authenticated = true;
+            this.#token = response.token;
+
+            return response.token
+        } else Error('Token is invalid')
+    }
+    async authenticateAsGuest() {
+        if (this.isAuthenticated()) throw Error('Already authenticated');
+        const uuid = uuidv4();
+        
+        const request = await fetch('https://beta.character.ai/chat/auth/lazy/', {
+            method:'POST',
+            body:JSON.stringify({
+                lazy_uuid: uuid
+            }),
+            headers: this.#guestHeaders
+        })
+        if (request.status === 200) {
+            const response = await request.json()
+            
+            if (response.success === true) {
+                this.#isGuest = true;
+                this.#authenticated = true;
+                this.#token = response.token;
+
+                return response.token;
+            } else throw Error('Registering failed');
+        } else throw Error('Failed to fetch a lazy token')
+    }
+    unauthenticate() {
+        if (this.isAuthenticated()) {
+            this.#authenticated = false;
+            this.#isGuest = false;
+            this.#token = undefined;
+        }
+    }
+
+    // getters
+    getToken() {
+        return this.#token;
+    }
+    isGuest() {
+        return this.#isGuest;
+    }
+    isAuthenticated() {
+        return (this.#authenticated)
+    }
+
+    // headers
+    getHeaders() {
+        return {
+            authorization: `Token ${this.#token}`,
+            'Content-Type': 'application/json',
+            //'sec-ch-ua': `"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"`,
+            //'sec-ch-ua-mobile': '?0'
+            /*'sec-ch-ua-platform': "Windows",
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',*/
+            //'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+        };
+    }
+}
+
+module.exports = Client
