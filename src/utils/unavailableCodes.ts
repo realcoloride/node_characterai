0fbe89902cfcad9167df33aee3ee8034dd14686a
Coloride <108619637+realcoloride@users.noreply.github.com>
2024-11-16 13:30:24 +0100

    update unavailable codes

--- src/utils/unavailableCodes.ts
+++ src/utils/unavailableCodes.ts
@@ -1,4 +1,4 @@
 export const CSRF_COOKIE_REQUIRED = "CSRF Cookie required for this feature";
 export const NO_DOMAIN_FOUND = "No domain found for this feature";
 export const NEEDS_MOBILE_DOMAIN = "Mobile domain not found yet";
-export const WEIRD_INTERNAL_SERVER_ERROR = "Internal Server Error; no solution has been found yet. Sorry!";
+export const WEIRD_INTERNAL_SERVER_ERROR = "Internal Server Error; no solution has been found yet.";
